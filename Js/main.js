aa2ace1365b5a83b1074d14bd3775a2d25a16b00
facucalvi <facundocalvimonte01@gmail.com>
2022-06-27 15:39:24 -0300

    cambios

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("Ingrese un numero del que desee saber su tabla"));
+
+
+for ( i = 1; i <= 20; i++ ){
+
+    console.log(numero + " x " + i + " es igual a " + numero*i);
+}
+
+// } if(numero*i == 15);
+//   console.log("No se puede seguir multiplicando la tabla");
+
